bdc56ff5212b21d11e72849c44e43338341f9407
Unknown <marcusviniciusdantasoliveira@gmail.com>
2018-08-23 00:22:15 -0300

    Adicionando novos componentes a estrutura
    Componentes
    SearchBar.jsx
    ListOfCharacters.jsx

---
+++ src/pages/sections/home/ListOfCharacters.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListOfCharacters extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (<div>Aqui vem a lista com os carinhas</div>);
+    }
+}
+ 
+export default ListOfCharacters;
---
+++ src/pages/sections/home/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (<div>Aqui vem o campo de pesquisas</div>);
+    }
+}
+ 
+export default SearchBar;
