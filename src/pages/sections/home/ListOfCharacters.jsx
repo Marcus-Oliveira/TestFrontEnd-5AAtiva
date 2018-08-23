d89ecba9613c9f8ccbf34b31183a53b5bd4361b1
Unknown <marcusviniciusdantasoliveira@gmail.com>
2018-08-23 02:31:10 -0300

    Isolando card em componente separado

--- src/pages/sections/home/ListOfCharacters.jsx
+++ src/pages/sections/home/ListOfCharacters.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from '../../../components/Card.jsx'
 
 class ListOfCharacters extends Component {
     constructor(props) {
@@ -6,21 +7,11 @@ class ListOfCharacters extends Component {
         this.state = {  }
     }
     render() { 
-        //return (<div>Aqui vem a lista com os carinhas - Fazer foreach do componente Card do Bootstrap</div>);
         return (
             <div style={{display: 'flex', justifyContent: 'space-around'}}>
-                <div className="card" style={{width: '18rem'}}>
-                    <img className="align-self-center card-img--circle" src="https://fakeimg.pl/56x56/" alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">SpiderMan</h5>
-                        <h6 class="card-subtitle mt-4">Peter Paker</h6>
-                        <p className="card-text">Descrição do Personagem Lorem ipsum dolor sit amet, consectetur </p>
-                    </div>
-                </div>
-
-                
-            </div>
-            
+                <Card />
+                <Card />
+            </div>           
         )
     }
 }
